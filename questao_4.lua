bd4496982f6244abaef145b4687d82a531451710
Wilton Silva <wrs@cin.ufpe.br>
2023-04-19 11:51:12 -0300

    Corrige código da questão 4

--- questao_4.lua
+++ questao_4.lua
@@ -13,43 +13,55 @@ local function random_attack()
     end
 end
 
-local function battle(pikachu_hp, raichu_hp)
+local function pikachu_attack(pikachu_hp, raichu_hp)
     local turn = 0
-
-    while pikachu_hp > 0 and raichu_hp > 0 do
+    while true do
         turn = turn + 1
-        local attack_name, damage
-
-        if turn % 2 == 1 then
-            attack_name, damage = random_attack()
-            raichu_hp = raichu_hp - damage
-            print("Turno", turn, "Pikachu usou", attack_name, "causando", damage, "de dano.")
-        else
-            attack_name, damage = random_attack()
-            pikachu_hp = pikachu_hp - damage
-            print("Turno", turn, "Raichu usou", attack_name, "causando", damage, "de dano.")
+        local attack_name, damage = random_attack()
+        raichu_hp = raichu_hp - damage
+        print("Turno", turn, "Pikachu usou", attack_name, "causando", damage, "de dano.")
+        if raichu_hp <= 0 then
+            return 0
         end
-
-        print("Pikachu HP:", pikachu_hp, "Raichu HP:", raichu_hp)
-        
-        if pikachu_hp <= 0 then
-            print("Raichu ganhou a batalha!")
-        elseif raichu_hp <= 0 then
-            print("Pikachu ganhou a batalha!")
-        end
-        
-        coroutine.yield()
+        coroutine.yield(raichu_hp)
     end
 end
 
-local battle_coroutine = coroutine.create(battle)
+local function raichu_attack(pikachu_hp, raichu_hp)
+    local turn = 0
+    while true do
+        turn = turn + 1
+        local attack_name, damage = random_attack()
+        pikachu_hp = pikachu_hp - damage
+        print("Turno", turn, "Raichu usou", attack_name, "causando", damage, "de dano.")
+        if pikachu_hp <= 0 then
+            return 0
+        end
+        coroutine.yield(pikachu_hp)
+    end
+end
+
+local pikachu_coroutine = coroutine.create(pikachu_attack)
+local raichu_coroutine = coroutine.create(raichu_attack)
 
 math.randomseed(os.time())
 
 local pikachu_hp = 800
 local raichu_hp = 1000
 
-coroutine.resume(battle_coroutine, pikachu_hp, raichu_hp)
-while coroutine.status(battle_coroutine) ~= "dead" do
-    coroutine.resume(battle_coroutine)
+local turn = 0
+while coroutine.status(pikachu_coroutine) ~= "dead" and coroutine.status(raichu_coroutine) ~= "dead" do
+    turn = turn + 1
+    if turn % 2 == 1 then
+        _, raichu_hp = coroutine.resume(pikachu_coroutine, pikachu_hp, raichu_hp)
+    else
+        _, pikachu_hp = coroutine.resume(raichu_coroutine, pikachu_hp, raichu_hp)
+    end
+    print("Pikachu HP:", pikachu_hp, "Raichu HP:", raichu_hp)
+end
+
+if pikachu_hp <= 0 then
+    print("Raichu ganhou a batalha!")
+elseif raichu_hp <= 0 then
+    print("Pikachu ganhou a batalha!")
 end
